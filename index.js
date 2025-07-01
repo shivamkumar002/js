8a2e8468600d38b40bc05965ccc29083e3e0bdb6
Shivam <kumarshivam49060@gmail.com>
2025-07-01 04:05:40 +0000

    datatypes

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 
-console.log("hey")
+// datatypes:-
 
-let age =10;
-if(age>=18){
-    console.log(" Hurrah!! you can do sex .")
-
-}
-else if(age<=12){
-    console.log("You r child")
-}
-else if(age>13.<18){
-    console.log("You r under age")
-}
-
-    
-
-else {
-    console.log(" Sorry , You can't do sex .")
-}
-    
-
-a = 6;
-b = 8;
-let c= a>b ?(a-b):(b-a);
-console.log(c); 
+// number 18
+// string- "shivam "
+// boolean-true/false
+// null 
+// undefined - the value is not define 
+// symbol-unique 
