c248d3a1f6f50b71405fabac534c32506f5d62d4
Shivam <kumarshivam49060@gmail.com>
2025-07-03 04:28:56 +0000

    comparison of data types

---
+++ basicsJS/03operation.js
@@ -0,0 +1,20 @@
+let negvalue = -value 
+// console.log(negvalue);
+
+// console.log(2*2);
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1="hello"
+
+let str2 = "shivam"
+
+let str3=str1+str2
+console.log(str3);
+
+
+ 
